e628b189ac6ed1d3eb5e754c9afccab4f9fb5c04
github username <github email address>
2018-11-08 09:33:39 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :doctors
-  resources :patients
-  resources :appointments, only: [:show]
+
+
+  resources :doctors, only: %i[index show]
+  resources :patients, only: %i[index show]
+  resources :appointments, only: %i[show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
